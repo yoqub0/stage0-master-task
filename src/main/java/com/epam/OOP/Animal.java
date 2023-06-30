d824e8d8c9cee36ce5e1e9e4b0a8baa3d22a85b4
yoqubdev <yyboboraximov@gmail.com>
2023-06-30 05:27:03 +0500

    Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,23 @@
 package com.epam.OOP;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Animal {
+
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public String getDescription() {
+        String pawString = (numberOfPaws == 1) ? "paw" : "paws";
+        String furString = hasFur ? "a" : "no";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + pawString + " and " + furString + " fur.";
+    }
+
 
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        String originalDescription = super.getDescription();
+        return originalDescription + " Moreover, it has 2 wings and can fly.";
+    }
 }
